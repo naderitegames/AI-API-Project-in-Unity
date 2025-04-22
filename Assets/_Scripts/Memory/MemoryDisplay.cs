37cad2e89cf5167137f1a0b07ede7aaad6f77044
nadderror <naderghaliany@gmail.com>
2025-04-22 10:39:26 +0330

    Input texts wraping fixed

--- Assets/_Scripts/Memory/MemoryDisplay.cs
+++ Assets/_Scripts/Memory/MemoryDisplay.cs
@@ -7,6 +7,8 @@ namespace _Scripts
 {
     public class MemoryDisplay : MonoBehaviour
     {
+        [SerializeField] private int charLimitationForDescription;
+        [SerializeField] private int charLimitationForTitle;
         [SerializeField] RTLTextMeshPro descriptionPlace;
         [SerializeField] RTLTextMeshPro titlePlace;
         [SerializeField] RTLTextMeshPro creationTimePlace;
@@ -28,11 +30,29 @@ namespace _Scripts
         {
             if (_memory != null)
             {
-                titlePlace.text = _memory.Title;
-                descriptionPlace.text = _memory.Description;
+                titlePlace.text = GetShortPreview(_memory.Title, charLimitationForTitle);
+                var targetDescription = _memory.Summary == "" ? _memory.Description : _memory.Summary;
+                descriptionPlace.text = GetShortPreview(targetDescription, charLimitationForDescription);
                 creationTimePlace.text = _memory.LastUpdateTime.ToString(CultureInfo.InvariantCulture);
                 lastModifiedTimePlace.text = _memory.LastUpdateTime.ToString(CultureInfo.InvariantCulture);
             }
+        }
+
+        public static string GetShortPreview(string text, int maxCharacters = 150)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return "";
+
+            if (text.Length <= maxCharacters)
+                return text;
+
+            // برای اینکه کلمات نصفه نشکنن
+            string shortened = text.Substring(0, maxCharacters);
+            int lastSpace = shortened.LastIndexOf(' ');
+
+            if (lastSpace > 0)
+                shortened = shortened.Substring(0, lastSpace);
+
+            return shortened + " ...";
         }
     }
 }
